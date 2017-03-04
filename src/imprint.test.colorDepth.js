4f980f0c130f2e743fe030344da68123e12b9e04
Matt Brailsford <me@mattbrailsford.com>
2017-03-04 19:33:21 +0000

    Workaround to 24/32 bit color inconstistancies

--- src/imprint.test.colorDepth.js
+++ src/imprint.test.colorDepth.js
@@ -4,7 +4,16 @@
 
 	imprint.registerTest("colorDepth", function(){
 		return new Promise(function(resolve) {
-			return resolve(screen.colorDepth || "");
+			var cd = screen.colorDepth;
+
+			// Some browsers return 24 rather than 32 as 32 is really
+			// 24 bit color depth + 8 bits alpha, so they see the alpha
+			// as not really being "color" so report 24 instead. 
+			// For consistancy, treat all 32 color depths as 24.
+			if (cd === 32)
+				cd = 24;
+
+			return resolve(cd || "");
 		});
 	});
 
