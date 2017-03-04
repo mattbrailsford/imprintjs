2e19c6da2aa7a1054148f5b9e11ebbfb105336f3
Matt Brailsford <me@mattbrailsford.com>
2017-03-04 09:08:26 +0000

    Create imprint.test.adBlocker.js

---
+++ src/imprint.test.adBlocker.js
@@ -0,0 +1,29 @@
+(function(scope){
+
+	'use strict';
+
+	ImprintJs.registerTest("adBlocker", function(){
+		return new Promise(function(resolve) {
+      var adsbox = document.createElement('div');
+      adsbox.innerHTML = '&nbsp;';
+      adsbox.className = 'adsbox';
+      adsbox.style.display = 'block';
+      adsbox.style.position = 'absolute';
+      adsbox.style.top = '0px';
+      adsbox.style.left = '-9999px';
+      try
+      { 
+        // body may not exist, that's why we need try/catch
+        document.body.appendChild(adsbox);
+        window.setTimeout(function() {
+          var result = adsbox.offsetHeight === 0;
+          document.body.removeChild(adsbox);
+          return resolve(result);
+        }, 10);
+      } catch (e) {
+        return resolve(false);
+      }
+		});
+	});
+
+})(window);
